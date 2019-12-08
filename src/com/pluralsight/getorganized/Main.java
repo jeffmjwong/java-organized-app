814fe2fe22fe28adcd5ef7e2475aa3da53c5f12e
Jeff Wong <v-jwong1@expedia.com>
2019-12-08 18:08:13 +1000

    add line of code

--- src/com/pluralsight/getorganized/Main.java
+++ src/com/pluralsight/getorganized/Main.java
@@ -3,6 +3,6 @@ package com.pluralsight.getorganized;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Organized package name!");
     }
 }

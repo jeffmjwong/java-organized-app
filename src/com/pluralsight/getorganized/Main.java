0c06a3d85a06f7d13f765abf26441de07919fbee
Jeff Wong <v-jwong1@expedia.com>
2019-12-09 19:43:40 +1000

    double ampersand

--- src/com/pluralsight/getorganized/Main.java
+++ src/com/pluralsight/getorganized/Main.java
@@ -3,6 +3,11 @@ package com.pluralsight.getorganized;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Organized package name!");
+        double number = Math.random() * 10;
+        if (number != 0 && 5/number == 1) {
+	        System.out.println(number);
+        } else {
+            System.out.println("oops");
+        }
     }
 }
